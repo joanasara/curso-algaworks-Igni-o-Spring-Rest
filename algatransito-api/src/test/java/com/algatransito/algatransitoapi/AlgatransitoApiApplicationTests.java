45ebcc4ab078a9b21e057fef28b0cda7583174cb
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2023-12-06 15:19:02 -0300

    Construindo a estrutura para iniciar a api

---
+++ algatransito-api/src/test/java/com/algatransito/algatransitoapi/AlgatransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algatransito.algatransitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgatransitoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
