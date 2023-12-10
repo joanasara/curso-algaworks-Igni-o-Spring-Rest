34c060bf25eb06ea95c913dd580b07ba7d5df37d
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2023-12-09 21:01:39 -0300

    Criacao da classe controller e a entidade

---
+++ algatransito-api/src/main/java/com/algatransito/algatransitoapi/controller/ProprietarioController.java
@@ -0,0 +1,23 @@
+package com.algatransito.algatransitoapi.controller;
+
+import com.algatransito.algatransitoapi.domain.model.Proprietario;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ProprietarioController {
+
+
+    @GetMapping("/proprietarios")
+    public List<Proprietario> listar() {
+        Proprietario proprietario01 = new Proprietario();
+        proprietario01.setNome("joana");
+        proprietario01.setTelefone("87447567567");
+        proprietario01.setEmail("joanasousa@gmail.com");
+
+        return List.of(proprietario01);
+    }
+}
---
+++ algatransito-api/src/main/java/com/algatransito/algatransitoapi/domain/model/Proprietario.java
@@ -0,0 +1,13 @@
+package com.algatransito.algatransitoapi.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Proprietario {
+
+    private String nome;
+    private String email;
+    private String telefone;
+}
