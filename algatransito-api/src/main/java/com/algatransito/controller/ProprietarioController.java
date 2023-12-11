098e39e5cd3ecaeaf90b27d45d82cf3ee0f87384
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2023-12-11 15:22:31 -0300

    Criando o repository jpa ProprietarioRepository.java

--- algatransito-api/src/main/java/com/algatransito/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algatransito/controller/ProprietarioController.java
@@ -1,22 +1,25 @@
 package com.algatransito.controller;
 
 import com.algatransito.domain.model.Proprietario;
+import com.algatransito.domain.repository.ProprietarioRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
+
+    //@Autowired ESTA ANOTACAO ELA DEFINE QUE QUEREMOS INJETAR UMA INSTACIA CRIADA PELO SPRING
+    private final ProprietarioRepository proprietarioRepository;
 
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        Proprietario proprietario01 = new Proprietario();
-        proprietario01.setNome("joana");
-        proprietario01.setTelefone("87447567567");
-        proprietario01.setEmail("joanasousa@gmail.com");
-
-        return List.of(proprietario01);
+        return proprietarioRepository.findAll();
     }
 }
