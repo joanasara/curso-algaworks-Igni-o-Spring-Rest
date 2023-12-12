c949649cac9997f4d7708c4f87a522027cbed05d
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2023-12-11 21:16:01 -0300

    Implementando endpoint de inclusão de recurso

--- algatransito-api/src/main/java/com/algatransito/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algatransito/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.algatransito.controller;
 import com.algatransito.domain.model.Proprietario;
 import com.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,10 +21,17 @@ public class ProprietarioController {
         return proprietarioRepository.findAll();
 
     }
+
     @GetMapping("/{proprietarioId}")
     public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 }
